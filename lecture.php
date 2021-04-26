c47133c3c31ca74c11edecd9499b4cf61b4eef10
Fernando Pante <72923212+alexia1029@users.noreply.github.com>
2021-04-27 06:22:16 +0800

    Add files via upload

---
+++ lecture.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+/*
+    $names=["john","Ferdz","Mary"];
+    echo $names[count($names)-1];  //to access data in array
+
+
+
+  $counter = 0;
+  while ($counter < 10){
+      echo "The count is: $counter<br/>";
+      $counter ++;
+  }
+  
+   
+    $counter = 0;
+    do {
+        echo "The count is: $counter<br/>";
+        $counter ++;
+    } while ($counter < 10)   
+
+     
+    for ($count = 0; $count <= 10; $count ++){
+
+    }
+
+    //FOR EACH USE IN ARRAY
+    $names=["john","Ferdz","Mary"];
+
+    foreach ($names as $value){
+        echo "$value <br/>";
+    }
+
+
+
+    //FUNCTIONS
+
+    function helloThere($name){
+        echo "Hello there $name";
+        
+    }
+    helloThere("Ferdz");
+
+
+
+function helloThere($first_name, $last_name){
+     "Hello there $first_name, $last_name" ;
+     return;
+    
+}
+echo helloThere("Ferdz", "Pante");
+
+*/
+
+//INCLUDE FUNCTION
+
+
+
+    ?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,5 @@
+<?php 
+    $company_name = "Guitar Magazine, Inc.";
+    $stagline = "Teaching Guitar since 2001";
+
+?>
